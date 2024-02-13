d466e57dc7678865196e669b6f0b0540fcdcb352
kaoren1 <yaroslavrusakevich@gmail.com>
2024-02-13 14:46:10 +0300

    a

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    double array[3][5];
+    double m[3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << "Введите элемент массива: ";
+            cin >> array[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < 5; j++) {
+            sum = sum + array[i][j];
+        }
+        m[i] = sum / 5;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cout << "\nСреднее арифметическое: " << m[i] << endl;
+    }   
+
+    return 0;
+}
